2f43705183819f074631dea0ff47d10e8416fdfa
lijunjin <lijunjin@static-192-165-1-57.cust.crystone.se>
2019-05-07 16:29:37 +0800

    feat(card): 新增系统多语言

--- excel_to_json/global/e2j.js
+++ excel_to_json/global/e2j.js
@@ -1,10 +1,10 @@
 const convertExcel = require('excel-as-json').processFile;
 const options = {
-  "sheet":'sheet2',
+  "sheet":'sheet1',
   "isColOriented": true,
   "omitEmtpyFields": true
 }
-convertExcel('global_lang.xlsx', 'motion.json', options, (err, data) => {
+convertExcel('global_lang.xlsx', 'total.json', options, (err, data) => {
   console.log("err", err)
   console.log("data", data)
 });
